1cd5903c59b57e57948a3c88dc0f5b1a51e0e9a3
Jacob Stiffler <j-stiffler@ti.com>
2020-04-24 15:52:26 -0400

    servo_drive_demo: add safety_app and add m4 support for am64x
    * The linking fails with "error: no suitable entry-point found; setting to 0"
    
    Signed-off-by: Jacob Stiffler <j-stiffler@ti.com>

---
+++ apps/servo_drive_demo/safety_app/src/main.c
@@ -0,0 +1,70 @@
+/*
+ *  Copyright (C) 2020 Texas Instruments Incorporated
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "safety_app_if.h"
+
+/*
+ * Implements EtherCAT R5F main().
+ * Control comes here immediately after the core boots-up
+ * All Motor control related APIs shall be called from this here.
+ */
+
+/* Added for debug purpose when load and run via SBL.
+ * set enableDebug = 1 and build for debug.
+ * Once started running connect CCS and reset enableDebug=0
+ * to proceed with single-step from the beginning
+ */
+void StartupEmulatorWaitFxn (void)
+{
+    volatile uint32_t enableDebug = 0;
+    do
+    {
+    }while (enableDebug);
+}
+
+int main(void)
+{
+    /* This is for debug purpose - see the description of function header */
+    StartupEmulatorWaitFxn();
+
+    printf ("M4F core0 is up !!!! \n");
+
+    while(1)
+    {
+    }
+}
+
