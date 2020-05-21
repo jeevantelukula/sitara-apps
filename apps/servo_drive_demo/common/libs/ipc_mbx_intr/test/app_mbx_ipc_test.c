9e3eae18ac931efd0da8676d5a18645a9de38d36
Shiju Sivasankaran <a0756868@ti.com>
2020-05-21 19:21:00 +0530

    servo_drive_demo: libs: add standalone IPC test application to validate MBX IPC between R5F0 and R5F1
    Sample Application is created in such a way that it mimics the EtherCAT behavior, sends sample motor control parameters in every 1ms
    Tested across 2 R5Fs of AM65xx with configurations as R5F1(TIRTOS) <-> R5F2 (BM)
    10K messages are send to each direction and checked at the receiver side
    
    Updated sample application to send/receive different messages on both R5Fs
    
    JIRA: SITSW-181

---
+++ apps/servo_drive_demo/common/libs/ipc_mbx_intr/test/app_mbx_ipc_test.c
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2020, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+
+/* ========================================================================== */
+/*                             Include Files                                  */
+/* ========================================================================== */
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <ti/csl/csl_types.h>
+#include <ti/csl/csl_mailbox.h>
+#include <ti/csl/soc.h>
+#include <ti/csl/arch/csl_arch.h>
+#include <ti/osal/CacheP.h>
+#include <ti/osal/HwiP.h>
+
+#include <app_log.h>
+#include <app_mbx_ipc.h>
+#include <app_sciclient.h>
+#include <app_mbx_ipc_test.h>
+
+
+/* ========================================================================== */
+/*                            Global Variables                                */
+/* ========================================================================== */
+
+static app_mbxipc_test_obj_t gTestMsgObj={0};
+
+/* ========================================================================== */
+/*                          Function Definitions                              */
+/* ========================================================================== */
+
+/*
+ * Please note that the test application works only between a pair of CPus
+ * Not more than 2 CPUs are supported, as each CPU can receive message
+ * from one remote CPU only.
+ */
+
+void mbxIpcMsgTestHandler (uint32_t src_cpu_id, uint32_t payload)
+{
+    test_msg_obj_t *rxobj = &gTestMsgObj.receiveObj;
+    test_msg_obj_t *payload_ptr = (test_msg_obj_t*)payload;
+
+    CacheP_Inv(payload_ptr, sizeof(test_msg_obj_t));
+    *rxobj = *payload_ptr;
+    gTestMsgObj.srcCpuId = src_cpu_id;
+    gTestMsgObj.isMsgReceived = 1;
+}
+
+int checkStatus (void)
+{
+    uintptr_t key;
+    int32_t status = -1;
+    test_msg_obj_t *rxobj = &gTestMsgObj.receiveObj;
+
+    while (1!=gTestMsgObj.isMsgReceived);
+
+    key = HwiP_disable();
+    gTestMsgObj.isMsgReceived = 0;
+    HwiP_restore(key);
+    if (gTestMsgObj.srcCpuId < MAILBOX_IPC_MAX_PROCS)
+    {
+        if ((rxobj->i32Velocity == VELOCITY+gTestMsgObj.srcCpuId) &&
+            (rxobj->i32Position == POSITION+gTestMsgObj.srcCpuId) &&
+            (rxobj->i16State == STATE+gTestMsgObj.srcCpuId))
+        {
+            status = 0;
+        }
+    }
+    gTestMsgObj.srcCpuId = 0xFF;
+    memset(rxobj, 0, sizeof(test_msg_obj_t));
+
+    return status;
+}
+
+int32_t ipcTestRun(int32_t iterationCnt)
+{
+    int32_t status = 0;
+    uint16_t remoteId;
+    uint32_t selfId = appMbxIpcGetSelfCpuId();
+    test_msg_obj_t *txobj = &gTestMsgObj.sendObj;
+    uint32_t payload = (uint32_t)txobj;
+
+    /* Communicate with each of the other cores */
+    for (remoteId = 0; remoteId < MAILBOX_IPC_MAX_PROCS; remoteId++)
+    {
+        if (remoteId == selfId)
+        {
+            continue;
+        }
+        if (!appMbxIpcIsCpuEnabled(remoteId))
+        {
+            continue;
+        }
+
+        txobj->i32Velocity = VELOCITY+selfId;
+        txobj->i32Position = POSITION+selfId;
+        txobj->i16State = STATE+selfId;
+        CacheP_wb(txobj, sizeof(test_msg_obj_t));
+
+        appMbxIpcSendNotify(remoteId, payload);
+    }
+
+    if (MAX_ITERATION_COUNT > iterationCnt)
+    {
+        status = checkStatus();
+    }
+
+    return status;
+}
+
+/********************************* End of file ******************************/
+
---
+++ apps/servo_drive_demo/common/libs/ipc_mbx_intr/test/tirtos/concerto.mak
@@ -0,0 +1,85 @@
+# Concerto is a simple single-pass, non-recursive, full dependency-managed
+# Makefile system. This concerto.mak file specifies a target module in the
+# Ccncerto build system.
+
+
+# This is a TI RTOS (SYSBIOS) based application developed specifically for the
+# R5F CPU architecture. This must be filtered here so that concerto does not
+# attempt any other combinations.
+ifeq ($(TARGET_OS),SYSBIOS)
+ifeq ($(TARGET_CPU),R5F)
+
+# Begin the concerto module declarations by includng the "PRELUDE"
+include $(PRELUDE)
+
+# Define object name (TARGET) and type (TARGET_TYPE)
+TARGET      := app_tirtos_mcu1_0_mailbox_ipc_test
+TARGETTYPE  := exe
+
+# Provide list of C files by using built-in macro
+CSOURCES    := $(call all-c-files)
+CSOURCES    += ../app_mbx_ipc_test.c
+ASSEMBLY    := copyVecsToATCM.S
+
+# Define application's root directory
+APPDIR := $(abspath $(SDIR)/../../../../..)
+
+# Add directory to include search path
+IDIRS+=$(APPDIR)/common/libs/logs/include
+IDIRS+=$(APPDIR)/common/libs/sciclient/include
+IDIRS+=$(APPDIR)/common/libs/ipc_mbx_intr/include
+IDIRS+=$(APPDIR)/common/libs/ipc_mbx_intr/test
+
+# Define core ID as each core will host an application that provides a unique
+# role in the system demo. This is beyond the concerto concept of TARGET_CPU,
+# so define this here.
+DEFS+=CPU_mcu1_0
+
+# The PDK libraries may also be unique for individual core instances.
+PDK_CORE_ID = mcu1_0
+
+# common "config" dependencies
+COMMON_CONFIG = $(abspath $(APPDIR)/common/config/$(SITARA_DEMO_SOC))
+
+# Append to STATIC_LIBS for common demo libraries
+# These must also be built using concerto, and concerto will handle the
+# dependencies
+#STATIC_LIBS += app_common_mcu1_0
+STATIC_LIBS += app_libs_logs
+STATIC_LIBS += app_libs_sciclient
+STATIC_LIBS += app_servo_drive_common_ipc_mbx_intr
+
+# Append to ADDITIONAL_STATIC_LIBS for external libraries (e.g. PDK)
+ADDITIONAL_STATIC_LIBS += ti.osal.aer5f
+ADDITIONAL_STATIC_LIBS += ti.csl.init.aer5f
+ADDITIONAL_STATIC_LIBS += ti.csl.aer5f
+ADDITIONAL_STATIC_LIBS += ti.board.aer5f
+ADDITIONAL_STATIC_LIBS += sciclient.aer5f
+ADDITIONAL_STATIC_LIBS += ti.drv.uart.aer5f
+
+# Add run-time libraries from toolchain
+SYS_STATIC_LIBS += rtsv7R4_T_le_v3D16_eabi
+
+# The following XDC_* files are used to configure SYSBIOS to enable TI RTOS.
+# XDC/BIOS configuro need below 3 input arguments to work
+#- BUILD File
+#- CFG File
+#- Linker CMD file
+XDC_BLD_FILE = $(APPDIR)/ethercat_loop/tirtos/config_r5f.bld
+XDC_IDIRS    = $(APPDIR)/ethercat_loop/tirtos/
+XDC_CFG_FILE = $(APPDIR)/ethercat_loop/tirtos/mcu1_0.cfg
+
+# The default XDC_PLATFORM is provided by the concerto build target, and based
+# on TARGET_PLATFORM and TARGET_CPU
+XDC_PLATFORM = $(SITARA_XDC_PLATFORM)
+
+# Set the linker.cmd files that specify linker options along with memory
+# placement.
+LINKER_CMD_FILES +=  $(COMMON_CONFIG)/mem_map/linker_mem_map.cmd
+LINKER_CMD_FILES +=  $(APPDIR)/ethercat_loop/src/linker.cmd
+
+# End concerto module declarations
+include $(FINALE)
+
+endif
+endif
