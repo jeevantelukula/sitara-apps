0e11e362003d05036f2513ebe8ba88285760a7f8
Shiju Sivasankaran <a0756868@ti.com>
2021-01-21 21:33:41 +0530

    OOB demo makefile update
    LCPD modified AM64x DTB name from k3-am64-base-board.dtb to
    k3-am642-evm.dtb

--- bin/dtb_boot_args.sh
+++ bin/dtb_boot_args.sh
@@ -1,5 +1,5 @@
 # DTB File to Modify
-LINUX_DTB=${PLSDK_PATH}/board-support/prebuilt-images/k3-am64-base-board.dtb
+LINUX_DTB=${PLSDK_PATH}/board-support/prebuilt-images/k3-am642-evm.dtb
 
 # Default boot args to use in the DTB. Update these as needed.
 BOOTARGS="console=ttyS2,115200n8 earlycon=ns16550a,mmio32,0x02800000 rw rootwait rootfstype=ext4 root=/dev/mmcblk1p2"
