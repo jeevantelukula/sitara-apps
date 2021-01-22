fa58ff967c1df5d5f966a2b30dd5b36cef7c4960
Dillon Frank <d-frank@ti.com>
2021-01-22 08:47:08 -0600

    makefile: Pass DTB as argument to DTB script
    - Pass the DTB_IMG variable as an argument to the
      DTB modification script
    - In the DTB modification script, added support for
      parsing for parsing the incoming argument for just
      the file path
    
    Signed-off-by: Dillon Frank <d-frank@ti.com>

--- bin/dtb_boot_args.sh
+++ bin/dtb_boot_args.sh
@@ -1,5 +1,8 @@
-# DTB File to Modify
-LINUX_DTB=${PLSDK_PATH}/board-support/prebuilt-images/k3-am642-evm.dtb
+#!/bin/bash
+
+# Parse for the DTB File to Modify
+DTB_LOAD_ARGS=(${1//,/ })
+LINUX_DTB=${DTB_LOAD_ARGS[1]}
 
 # Default boot args to use in the DTB. Update these as needed.
 BOOTARGS="console=ttyS2,115200n8 earlycon=ns16550a,mmio32,0x02800000 rw rootwait rootfstype=ext4 root=/dev/mmcblk1p2"
